b857a46b40c051261fdf1ac633f2f10466fd86fe
Pete Smith <roysvork@gmail.com>
2015-05-08 21:51:30 +0100

    Rudimentary two way binding - from the value of an input box to the text property of a p tag

--- src/lib/data-binding.js
+++ src/lib/data-binding.js
@@ -1,17 +1,30 @@
 (function (dataBinding) {
 
-	var value = function (model, propertyName, selector) {
+	var toText = function (model, propertyName, selector) {
 		var element = document.querySelectorAll(selector)[0];
 		var value = model[propertyName];
 
 		Object.defineProperty(model, propertyName, {
 			get: function() { return value },
-			set: function(newValue) { element.setAttribute('value', newValue) }
+			set: function(newValue) { element.textContent = newValue }
 		});
 
 		model[propertyName] = value;
 	}
 
-	dataBinding.value = value;
+	dataBinding.to = {};
+	dataBinding.to.text = toText;
+
+	var fromValue = function(model, propertyName, selector) {
+		var element = document.querySelectorAll(selector)[0];
+		element.addEventListener("change", function (e) {
+			model[propertyName] = e.target.value;
+		});
+	}
+
+	dataBinding.from = {};
+	dataBinding.from.value = fromValue;
+
+
 
 })(window.dataBinding = window.dataBinding || {})
