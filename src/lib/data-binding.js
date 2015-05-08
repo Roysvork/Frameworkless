68fca9269859ab7c2cdee72671bcb3264bd34e3e
Pete Smith <roysvork@gmail.com>
2015-05-08 08:11:39 +0100

    Basic model -> element databinding for the value attribute

--- src/lib/data-binding.js
+++ src/lib/data-binding.js
@@ -1,5 +1,17 @@
 (function (dataBinding) {
 
-	
+	var value = function (model, propertyName, selector) {
+		var element = document.querySelectorAll(selector)[0];
+		var value = model[propertyName];
+
+		Object.defineProperty(model, propertyName, {
+			get: function() { return value },
+			set: function(newValue) { element.setAttribute('value', newValue) }
+		});
+
+		model[propertyName] = value;
+	}
+
+	dataBinding.value = value;
 
 })(window.dataBinding = window.dataBinding || {})
