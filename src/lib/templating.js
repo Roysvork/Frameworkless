fe793adadd69b02ce4eb5a28bb27c310f85c1496
Pete Smith <roysvork@gmail.com>
2015-06-03 21:39:55 +0100

    Improved the templating logic to make it easier to follow during the demo

--- src/lib/templating.js
+++ src/lib/templating.js
@@ -1,46 +1,44 @@
 (function (templating) {
 
-	var fluentTemplate = function (_template, _fn, _action) {
-		this.template = _template;
-		this.fn = _fn;
-		this.action = _action;
+	var fluentTemplate = function (templateSelector, init, container) {
+		this.template = (container || document).querySelector(templateSelector).content;
+		this.init = init;
 	}
 
 	fluentTemplate.prototype.createElement = function (model) {
-		return compose(document.importNode(this.template, true), function (el) {
-			this.fn(model, el.firstElementChild);
-		}.bind(this));
+		var element = document.importNode(this.template, true);
+		this.init(element.firstElementChild, model);
+		return element;
 	}
 
-	fluentTemplate.prototype.render = function (_data) {
-		this.data = _data;
+	fluentTemplate.prototype.render = function (data) {
+		this.data = data;
 		return this;
 	};
 
-	fluentTemplate.prototype.to = function (targetSelector, container) {
+	var toSingle = function (targetSelector, container) {
 		var target = (container || document).querySelector(targetSelector);
-		this.action(target);
+		target.appendChild(this.createElement(this.data));
+	};
+
+	var single = function(templateSelector, init, container) {
+		return compose(
+			new fluentTemplate(templateSelector, init, container),
+			function (tpl) { tpl.to = toSingle });
 	}
 
-	var single = function(templateSelector, fn, container) {
-		container = container || document;
-		var template = container.querySelector(templateSelector).content;
+	var toRepeat = function (targetSelector, container) {
+		var target = (container || document).querySelector(targetSelector);
+		forEach(this.data, function(item) {
+			target.appendChild(this.createElement(item));
+		}.bind(this));
+	}
 
-		return new fluentTemplate(template, fn, function (target) {
-			target.appendChild(this.createElement(this.data));
-		});
-	};
-
-	var repeat = function (templateSelector, fn, container) {
-		container = container || document;
-		var template = container.querySelector(templateSelector).content;
-
-		return new fluentTemplate(template, fn, function (target) {
-			forEach(this.data, function(item) {
-				target.appendChild(this.createElement(item));
-			}.bind(this));
-		});
-	};
+	var repeat = function(templateSelector, init, container) {
+		return compose(
+			new fluentTemplate(templateSelector, init, container),
+			function (tpl) { tpl.to = toRepeat });
+	}
 
 	templating.single = single;
 	templating.repeat = repeat;
