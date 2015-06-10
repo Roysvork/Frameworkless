458db7da735c9dfd2bd5eb8ab57ef43c08a107e0
Pete Smith <roysvork@gmail.com>
2015-06-10 08:13:40 +0100

    Further simplifications to templating code

--- src/lib/templating.js
+++ src/lib/templating.js
@@ -1,44 +1,41 @@
 (function (templating) {
-
-	var fluentTemplate = function (templateSelector, init, container) {
-		this.template = (container || document).querySelector(templateSelector).content;
+	
+	var fluentTemplate = function (templateSelector, init) {
+		this.template = document.querySelector(templateSelector).content;
 		this.init = init;
-	}
+	};
 
-	fluentTemplate.prototype.createElement = function (model) {
+	fluentTemplate.prototype.createElement = function (data) {
 		var element = document.importNode(this.template, true);
-		this.init(element.firstElementChild, model);
+		this.init(element.firstElementChild, data);
 		return element;
-	}
+	};
 
 	fluentTemplate.prototype.render = function (data) {
 		this.data = data;
 		return this;
 	};
 
-	var toSingle = function (targetSelector, container) {
-		var target = (container || document).querySelector(targetSelector);
-		target.appendChild(this.createElement(this.data));
+	var single = function (templateSelector, init) {
+		return compose(new fluentTemplate(templateSelector, init), function (tpl) {
+			tpl.to = function (targetSelector) {
+				var target = document.querySelector(targetSelector);
+				target.appendChild(this.createElement(this.data));
+			};
+		});
 	};
 
-	var single = function(templateSelector, init, container) {
-		return compose(
-			new fluentTemplate(templateSelector, init, container),
-			function (tpl) { tpl.to = toSingle });
-	}
-
-	var toRepeat = function (targetSelector, container) {
-		var target = (container || document).querySelector(targetSelector);
-		forEach(this.data, function(item) {
-			target.appendChild(this.createElement(item));
-		}.bind(this));
-	}
-
-	var repeat = function(templateSelector, init, container) {
-		return compose(
-			new fluentTemplate(templateSelector, init, container),
-			function (tpl) { tpl.to = toRepeat });
-	}
+	var repeat = function (templateSelector, init) {
+		return compose(new fluentTemplate(templateSelector, init), function (tpl) {
+			tpl.to = function (targetSelector) {
+				var target = document.querySelector(targetSelector);
+				target.innerHTML = "";
+				forEach(this.data, function (item) {
+					target.appendChild(this.createElement(item));					
+				}.bind(this));
+			};
+		});
+	};
 
 	templating.single = single;
 	templating.repeat = repeat;
