90609f9aba362d57aca26d473c26ec09d262d1de
Pete Smith <roysvork@gmail.com>
2015-06-17 10:48:34 +0200

    Databinding stage 2

--- src/lib/data-binding-2.js
+++ src/lib/data-binding-2.js
@@ -1,10 +1,45 @@
 (function (dataBinding) {
 	
+	var wrap = function(fn, model) {
+		fn.wrapped = true;
+		fn.actions = [];
+		fn.update = function() { 
+			var result = fn.bind(model)();
+			forEach(fn.actions, function (action) {
+				action(result);
+			});
+		};
+	
+		return fn;
+	};
+
+	var ensureWrapped = function (model, propertyName) {
+		var property = model[propertyName];
+		if (typeof property === "function") {
+			return property.wrapped ? property : wrap(property, model);
+		}
+
+		var getter = getGetter(model, propertyName);
+		if (getter && getter.wrapped) {
+			return getter;
+		}
+
+		getter = wrap(function() { return property; }, model);
+		Object.defineProperty(model, propertyName, {
+			get: getter,
+			set: function(newValue) { 
+				property = newValue;
+				getter.update();
+			}
+		});
+
+		return getter;
+	}
+
 	var fromValue = function (model, propertyName, elementSelector) {
 		var element = document.querySelector(elementSelector);
 		var fn = function (e) {
 			model[propertyName] = e.target.value;
-			alert(model[propertyName]);
 		}
 
 		element.addEventListener("change", fn);
@@ -12,11 +47,22 @@
 	};
 
 	var toText = function (model, propertyName, elementSelector) {
+		var element = document.querySelector(elementSelector);
+		var property = ensureWrapped(model, propertyName);
 
+		property.actions.push(function (newValue) {
+			element.textContent = newValue;
+		});
+
+		property.update();
 	};
 
 	dataBinding.from = {
 		value: fromValue
+	};
+
+	dataBinding.to = {
+		text: toText
 	};
 
 }(window.dataBinding = window.dataBinding || {}));
