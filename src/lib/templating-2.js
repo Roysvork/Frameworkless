5fcc8c79ab688696e0993e44b5fbc77147d86795
Pete Smith <roysvork@gmail.com>
2015-06-17 11:13:04 +0200

    Templating stage 1

--- src/lib/templating-2.js
+++ src/lib/templating-2.js
@@ -6,11 +6,25 @@
 	};
 
 	fluentTemplate.prototype.render = function (data) {
-		
+		this.data = data;
+		return this;
 	};
 
-	fluentTemplate.prototype.createElement = function () {
-
+	fluentTemplate.prototype.createElement = function (data) {
+		var element = document.importNode(this.template, true);
+		this.init(element.firstElementChild, data);
+		return element;
 	};
+
+	var single = function (templateSelector, init) {
+		return compose(new fluentTemplate(templateSelector, init), function (tpl) {
+			tpl.to = function (targetSelector) {
+				var target = document.querySelector(targetSelector);
+				target.appendChild(this.createElement(this.data));
+			}
+		})
+	};
+
+	templating.single = single;
 
 }(window.templating = window.templating || {}));
