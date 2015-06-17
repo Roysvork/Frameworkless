123397de43019ec92b80ef39c472d7d5e8a40805
Pete Smith <roysvork@gmail.com>
2015-06-17 10:43:12 +0200

    Databinding stage 1

--- src/lib/data-binding-2.js
+++ src/lib/data-binding-2.js
@@ -1,11 +1,22 @@
 (function (dataBinding) {
 	
 	var fromValue = function (model, propertyName, elementSelector) {
+		var element = document.querySelector(elementSelector);
+		var fn = function (e) {
+			model[propertyName] = e.target.value;
+			alert(model[propertyName]);
+		}
 
+		element.addEventListener("change", fn);
+		fn({ target: element });
 	};
 
 	var toText = function (model, propertyName, elementSelector) {
 
+	};
+
+	dataBinding.from = {
+		value: fromValue
 	};
 
 }(window.dataBinding = window.dataBinding || {}));
